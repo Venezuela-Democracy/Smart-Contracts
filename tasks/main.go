5a4803a29d4ba77908dd4a2c6c193302e662bb00
Noah Naizir <Itahand.naizir@gmail.com>
2024-12-14 11:16:16 -0500

    Testing framework

---
+++ tasks/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/bjartek/overflow/v2"
+	"github.com/fatih/color"
+)
+
+func main() {
+	// Comienza el programa overflow
+	o := Overflow(
+		WithGlobalPrintOptions(),
+	)
+	fmt.Println("Testing Vzla Democracy cadence contracts")
+	//
+	///// Probando CADENCE en Overflow /////
+	//
+	color.Red("Debería de incrementar el Contador")
+	// Incrementa el contador
+	o.Tx("IncrementCounter",
+		WithSigner("bob"),
+	)
+}
